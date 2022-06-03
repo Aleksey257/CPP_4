d4136692245ba9851f61139a67071ca1e2f30da5
Ilya Ivanov <malycheharovaleksey@gmail.com>
2022-06-03 11:06:52 +0900

    version_5

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    int a;
+    int b;
+    int c;
+
+    std::cout << "Введите первое число: ";
+    std::cin >> a;
+    std::cout << "Введите первое число: ";
+    std::cin >> b;
+    std::cout << "Cреднее арифметическое = " << (a + b)/2 << "\n";
+    std::cout << "Выбери знак операции (1 - Сложение, 2 - Вычитание, 3 - Умножение, 4 - Деление): ";
+    std::cin >> c;
+ //   if (c == 1)
+ //       std::cout << (a + b);
+ //   else if (c == 2)
+ //       std::cout << (a - b);
+ //   else if (c == 3)
+ //       std::cout << (a * b);
+ //   else if (c == 4)
+ //      std::cout << (a / b);
+ //  else
+ //       std::cout << "Неккоретный ввод";
+    switch (c) {
+    case 1:
+       std::cout << (a + b);
+        break;
+    case 2:
+        std::cout << (a - b);
+        break;
+     case 3:
+        std::cout << (a * b);
+        break;
+    case 4:
+        std::cout << (a / b);
+        break;
+    default:
+       std::cout << "Неккоретный ввод";
+    }
+    }
